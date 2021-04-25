8c1c835c25c54c05049a8df6f234531eaa9c9c74
Christian Bryant <74256156+Christian-Bryant2020@users.noreply.github.com>
2021-04-25 17:55:07 -0500

    Display Questions

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
-let secondsLeft = "90";
-let startTimer = document.querySelector("#timer")
-let time = document.querySelector("#time");
+let homePage = document.getElementById("homePage")
+let startButton = document.getElementById("start").addEventListener("click", questionOne);
+let questionTwoButton = document.getElementById("passq2").addEventListener("click", questionTwo);
+//let questionThreeButton = document.getElementById("passq3").addEventListener("click", questionThree);
 
-time = 15;
 
-function setTime() {
-    var timerInterval = setInterval(function() {
-      secondsLeft--;
-      timer.textContent = secondsLeft + " seconds left till colorsplosion.";
-  console.log(secondsLeft);
-      if(secondsLeft === 0) {
-        clearInterval(timerInterval);
-        sendMessage();
-      }
-    }, 1000);
-  }
+function questionOne() {
+  document.getElementById('homePage').innerHTML = document.getElementById('questionOne').innerHTML;
+    if(document.getElementById('q1c2').clicked)
+  console.log('yes')
+  else(
+  console.log('no'))
+}
+function questionTwo(){
+  document.getElementById('questionOne').innerHTML = document.getElementById('questionTwo').innerHTML;
+    if(document.getElementById('q1c2').clicked)
+  console.log('yes')
+  else(
+  console.log('no'))
+}
+
+// let questionOne = 
+// let secondsLeft = "90";
+// let startTimer = document.querySelector("#timer")
+// let time = document.querySelector("#time");
+
+// time = 15;
+
+// function setTime() {
+//     var timerInterval = setInterval(function() {
+//       secondsLeft--;
+//       timer.textContent = secondsLeft + " seconds left till colorsplosion.";
+//   console.log(secondsLeft);
+//       if(secondsLeft === 0) {
+//         clearInterval(timerInterval);
+//         sendMessage();
+//       }
+//     }, 1000);
+//   }
