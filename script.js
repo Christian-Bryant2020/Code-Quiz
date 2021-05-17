7fe1d998f4a730eed96a2e692dbe6cc9c3b935bf
Christian Bryant <74256156+Christian-Bryant2020@users.noreply.github.com>
2021-05-17 16:38:10 -0500

    Replaces question when started

--- script.js
+++ script.js
@@ -1,48 +1,51 @@
-let quizContainer = document.getElementById("quizContainer");
-let submitButton = document.getElementById('submit');
-let startButton = document.getElementById("start-btn");
-
-startButton.addEventListener("click", quizStart);
+const quizContainer = document.getElementById("quizContainer");
+const submitButton = document.getElementById('submit');
+const startButton = document.getElementById("start-btn");
+const welcomeContainer = document.getElementById("welcome-text")
+const answerButtonsElement = document.getElementById("answer-buttons")
+let questionElement = document.getElementById("question")
+let shuffledQuestions, currentQuestionIndex
 
 let questions = [
   {
-    question: "Words for question one",
-    options: {
-      1: 'one',
-      2: 'two',
-      3: 'three'
-    },
-    correctAnswer: '3'
-  },
-  {
-    question: "Words for question one",
-    options: {
-      1: 'one',
-      2: 'two',
-      3: 'three'
-    },
-    correctAnswer: '3'
-  },
-  {
-    question: "Words for question one",
-    options: {
-      1: 'one',
-      2: 'two',
-      3: 'three'
-    },
-    correctAnswer: '3'
-  }
-];
+     question: 'What is JavaScript?',
+     answers: [
+       { text: 'correct', correct: true },
+       { text: 'false', correct: false },
+       { text: 'false', correct: false },
+       { text: 'false', correct: false },
+     ]
+   }
+  ];
 
-function quizStart(){
-  quizContainer.innerHTML = questions[0].options[1];
-  console.log(quizContainer)
+startButton.addEventListener("click", quizStart);
+
+function quizStart() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  quizContainer.classList.remove('hide')
+  nextQuestion()
 };
 
-// funciton nextQuestion(){
-// };
+ function nextQuestion() {
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+};
 
-// function selectAnswer(){
-// };
+ function showQuestion(question) {
+   questionElement.innerText = questions.question
+  //  question.answers.forEach(answers => {
+  //    const button = document.createElement('button')
+  //    button.inerText = answers.textbutton.classList.add('btn')
+  //    if (answers.correct) {
+  //      button.dataset.correct = answers.correct
+  //    }
+  //    button.addEventListener('click', selectAnswer)
+  //    answerButtonsElement.appendChild(button)
+  //  })
+ }
+ 
 
-console.log(quizContainer)
+
+function selectAnswer(){
+};
