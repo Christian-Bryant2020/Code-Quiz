68b7ecf0fe03dc38df9f59e1f20cf8de0f357be5
Christian Bryant <74256156+Christian-Bryant2020@users.noreply.github.com>
2021-06-01 17:30:40 -0500

    Render question and answer btns

--- script.js
+++ script.js
@@ -1,54 +1,64 @@
 let quizContainer = document.getElementById("quizContainer");
 let submitButton = document.getElementById('submit');
-let startButton = document.getElementById("start");
+let startButton = document.getElementById("start-btn");
 let timer = document.getElementById("timer");
 let timerLeft = 60;
-
-startButton.addEventListener("click", quizStart);
-
+let welcomeText = document.getElementById("welcome-text")
+let nextBtn = document.getElementById("next-btn")
+let questionIndex = 0;
+let answers = document.getElementById("answer-options")
 let questions = [
   {
-    question: "Words for question one",
-    options: {
-      1: 'one',
-      2: 'two',
-      3: 'three'
-    },
-    correctAnswer: '3'
+    question: "This is the text for question one",
+    choices: ["Don't choose this", "Don't choose this", "Don't choose this", "correct"],
+    values: ["incorrect", "incorrect", "incorrect", "correct"]
   },
   {
-    question: "Words for question one",
-    options: {
-      1: 'one',
-      2: 'two',
-      3: 'three'
-    },
-    correctAnswer: '3'
+    question: "This is the text for question two",
+    choices: ["Don't choose this", "Don't choose this", "correct", "Don't choose this"],
+    values: ["incorrect", "incorrect", "correct", "incorrect"]
   },
   {
-    question: "Words for question one",
-    options: {
-      1: 'one',
-      2: 'two',
-      3: 'three'
-    },
-    correctAnswer: '3'
+    question: "This is the text for question three",
+    choices: ["Don't choose this", "Don't choose this", "correct", "Don't choose this"],
+    values: ["incorrect", "incorrect", "correct", "incorrect"]
+  },
+  {
+    question: "This is the text for question four",
+    choices: ["Don't choose this", "Don't choose this", "Don't choose this", "correct"],
+    values: ["incorrect", "incorrect", "incorrect", "correct"]
   }
 ];
 
 function quizStart(){
-  quizContainer.innerHTML = questions[0].options[1];
-  console.log(quizContainer)
+  console.log("hello")
+  startButton.classList.add("hide");
+  welcomeText.classList.add("hide");
+  nextBtn.classList.remove("hide");
+  quizContainer.textContent = questions[questionIndex].question; 
+
+  for(var i = 0; i < questions.length; i++){
+    answerChoices = document.createElement("btn");
+    brLine = document.createElement("br");
+    answerChoices.setAttribute("class","btn ");
+    answerChoices.setAttribute("value", questions[questionIndex].values[i]);
+    answerChoices.textContent= questions[questionIndex].choices[i];
+    answers.append(answerChoices);
+    answers.append(brLine);
+  }
+
+  function setTimmer(){
+    timeInterval = setInterval(function(){
+      timer.textContent = timerLeft;
+      timerLeft--;
+  
+      if(timerLeft === 0){
+        displayScores();
+      }
+    })
+  }
 };
 
-function setTimmer(){
-  timeInterval = setInterval(function(){
-    timer.textContent = timerLeft;
-    timerLeft--;
 
-    if(timerLeft === 0){
-      displayScores();
-    }
-  })
-}
-console.log(quizContainer)
+
+startButton.addEventListener("click", quizStart);
