3f91d76eccb0c91e39c860144f55b1ed17ab1e0e
Christian Bryant <74256156+Christian-Bryant2020@users.noreply.github.com>
2021-06-01 18:58:59 -0500

    Fixed cycling though questions
    Broke validation

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ let submitButton = document.getElementById('submit');
 let startButton = document.getElementById("start-btn");
 let timer = document.getElementById("timer");
 let timerLeft = 60;
+let timeInterval;
+let score = 0;
 let welcomeText = document.getElementById("welcome-text")
-let nextBtn = document.getElementById("next-btn")
-let questionIndex = 0;
+let arrayIndex = 0;
 let answers = document.getElementById("answer-options")
 let questions = [
   {
@@ -30,35 +31,77 @@ let questions = [
   }
 ];
 
-function quizStart(){
-  console.log("hello")
+
+function quizStart() {
   startButton.classList.add("hide");
   welcomeText.classList.add("hide");
-  nextBtn.classList.remove("hide");
-  quizContainer.textContent = questions[questionIndex].question; 
+  timer.classList.remove("hide");
+  quizContainer.textContent = questions[arrayIndex].question;
 
-  for(var i = 0; i < questions.length; i++){
+  for (var i = 0; i < questions.length; i++) {
     answerChoices = document.createElement("btn");
     brLine = document.createElement("br");
-    answerChoices.setAttribute("class","btn ");
-    answerChoices.setAttribute("value", questions[questionIndex].values[i]);
-    answerChoices.textContent= questions[questionIndex].choices[i];
+    answerChoices.setAttribute("class", "btn btn-outline-primary");
+    answerChoices.setAttribute("status", questions[arrayIndex].values[i]);
+    answerChoices.textContent = questions[arrayIndex].choices[i];
     answers.append(answerChoices);
     answers.append(brLine);
   }
-
-  function setTimmer(){
-    timeInterval = setInterval(function(){
-      timer.textContent = timerLeft;
-      timerLeft--;
-  
-      if(timerLeft === 0){
-        displayScores();
-      }
-    })
-  }
+  validateAnswer();
 };
 
+function validateAnswer() {
+  let allBtn = document.querySelectorAll(".btn-outline-primary")
+  allBtn.forEach(function (allBtn) {
+    allBtn.addEventListener("click", function () {
+      console.log(allBtn)
+      console.log(this.status)
 
+      if (this.status == "correct") {
+        alert("Correct!");
+        score++
+        arrayIndex++;
+        nextQuestion();
+      } else {
+        arrayIndex++;
+        alert("Incorrect!")
+        nextQuestion();
+      }
+    })
+  })
+};
 
-startButton.addEventListener("click", quizStart);
+function nextQuestion() {
+  if (arrayIndex < 4) {
+    quizContainer.textContent = questions[arrayIndex].question;
+    for (i = 0; i < questions.length; i++) {
+      document.getElementsByClassName("btn-outline-primary")[i].textContent = questions[2].choices[i];
+      document.getElementsByClassName("btn-outline-primary")[i].setAttribute("value", questions[2].values[i]);
+    }
+  }
+  else {
+    displayScores();
+  }
+}
+
+function displayScores() {
+  clearInterval(timeInterval);
+  quizContainer.textContent = ("GG! Your score is " + score);
+}
+
+function setTimmer() {
+  console.log(
+    'started'
+  )
+  timeInterval = setInterval(function () {
+    timer.textContent = "Time left: " + timerLeft;
+    timerLeft--;
+    if (timerLeft === 0) {
+      displayScores();
+    }
+  }, 1000);
+}
+startButton.addEventListener("click", function () {
+  quizStart();
+  setTimmer();
+});
